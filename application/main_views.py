c5dc2d2c8a3581e93ba42d258e65584357f185fb
Kas Barlow <invinsiblebaros@gmail.com>
2020-08-30 17:59:47 +0300

    more additions done with more pages created for all sectors. plus, graphs and maps added in there

--- application/main_views.py
+++ application/main_views.py
@@ -16,6 +16,62 @@ def home():
     return render_template("./main_pages/home.html")
 
 
-@app.route("/data")
-def data():
-    return render_template("./main_pages/data.html")
+@app.route("/accountability")
+def accountability():
+    return render_template("./main_pages/accountability.html")
+
+
+@app.route("/agriculture")
+def agriculture():
+    return render_template("./main_pages/agriculture.html")
+
+
+@app.route("/education")
+def education():
+    return render_template("./main_pages/education.html")
+
+
+@app.route("/energy_and_mineral_development")
+def energy_and_mineral_development():
+    return render_template("./main_pages/energy_and_mineral_development.html")
+
+
+@app.route("/health")
+def health():
+    return render_template("./main_pages/health.html")
+
+
+@app.route("/ict_and_national_guidance")
+def ict_and_national_guidance():
+    return render_template("./main_pages/ict_and_national_guidance.html")
+
+
+@app.route("/justice_law_and_order")
+def justice_law_and_order():
+    return render_template("./main_pages/justice_law_and_order.html")
+
+
+@app.route("/lands_and_housing")
+def lands_and_housing():
+    return render_template("./main_pages/lands_and_housing.html")
+
+
+@app.route("/public_sector_management")
+def public_sector_management():
+    return render_template("./main_pages/public_sector_management.html")
+
+
+@app.route("/science_technology_and_innovation")
+def science_technology_and_innovation():
+    return render_template("./main_pages/science_technology_and_innovation.html")
+
+
+@app.route("/social_development")
+def social_development():
+    return render_template("./main_pages/social_development.html")
+
+
+@app.route("/water_and_environment")
+def water_and_environment():
+    return render_template("./main_pages/water_and_environment.html")
+
