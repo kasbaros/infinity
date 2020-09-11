9a4ae46d332fe217fd4c2cc6e7eb91ca23d65d4e
Kas Barlow <invinsiblebaros@gmail.com>
2020-09-11 15:46:57 +0300

    This is the first commit maybe
    The ubos gender portal

--- application/education_map.py
+++ application/education_map.py
@@ -6,20 +6,20 @@ regions = os.path.join("./static/data", "uganda_subregion_boundaries.geojson")
 education_regions = os.path.join("./static/data", "education_data.csv")
 education_data = pd.read_csv(education_regions)
 
-m = folium.Map(location=[31.0123827609908, 0.1947430097058114], zoom_start=5)
+m = folium.Map(location=[0.347596, 32.582520], zoom_start=7)
 
-m.choropleth(
+folium.Choropleth(
     geo_data=regions,
     name="Choropleth",
     data=education_data,
     columns=["Geography", "2017 Male", "2017 Female"],
     key_on="feature.properties.SUB_REGION",
-    fill_color="YlGn",
+    fill_color="BuPu",
     fill_opacity=0.6,
     line_opacity=0.2,
     legend_name="Education among Genders %",
-)
+).add_to(m)
 
-folium.LayerControl().add_to(m)
+# folium.LayerControl().add_to(m)
 m.save("./templates/maps/education_map.html")
 
