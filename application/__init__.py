3a08ad614279846a657eb43a8f868c64e6c5861f
kasbaros <invinsiblebaros@gmail.com>
2020-09-17 22:01:23 +0300

    lots more additions

--- application/__init__.py
+++ application/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder="templates")
 app.config["SECRET_KEY"] = "11606633055e40cbe882f08f27fec5c2501f8978"
+app.config["SQLALCHEMY_ECHO"] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///genderstatsport.db"
 
 db = SQLAlchemy(app)
--- application/education_map.py
+++ application/education_map.py
@@ -6,7 +6,16 @@ regions = os.path.join("./static/data", "15_Statistical_Regions.geojson")
 education_regions = os.path.join("./static/data", "education_data.csv")
 education_data = pd.read_csv(education_regions)
 
-m = folium.Map(location=[0.347596, 32.582520], zoom_start=7)
+m = folium.Map(
+    location=[0.347596, 32.582520],
+    tiles="http://tile.stamen.com/toner/{z}/{x}/{y}.png ",
+    attr="toner-bcg",
+    zoom_start=7,
+)
+
+# tiles = ["stamenwatercolor", "cartodbpositron", "openstreetmap", "stamenterrain"]
+# for tile in tiles:
+#   folium.TileLayer(tile).add_to(m)
 
 folium.Choropleth(
     geo_data=regions,
@@ -14,7 +23,7 @@ folium.Choropleth(
     data=education_data,
     columns=["Geography", "2017 Male", "2017 Female"],
     key_on="feature.properties.F15Regions",
-    fill_color="BuPu",
+    fill_color="YlGn",
     fill_opacity=0.6,
     line_opacity=0.2,
     legend_name="Education among Genders %",
