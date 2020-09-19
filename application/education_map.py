fcbc635dd0d6eb9306ce27e0347ea749b5aafadf
kasbaros <invinsiblebaros@gmail.com>
2020-09-19 14:44:07 +0300

    Several changes made
    Lots of changes added on several pages including new sections for the news and event separated and more

--- application/education_map.py
+++ application/education_map.py
@@ -8,27 +8,31 @@ education_data = pd.read_csv(education_regions)
 
 m = folium.Map(
     location=[0.347596, 32.582520],
-    tiles="http://tile.stamen.com/toner/{z}/{x}/{y}.png ",
-    attr="toner-bcg",
+    tiles="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}",
+    attr="Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ",
     zoom_start=7,
 )
 
-# tiles = ["stamenwatercolor", "cartodbpositron", "openstreetmap", "stamenterrain"]
-# for tile in tiles:
-#   folium.TileLayer(tile).add_to(m)
+tiles = ["stamenwatercolor", "cartodbpositron", "openstreetmap", "stamenterrain"]
+for tile in tiles:
+    folium.TileLayer(tile).add_to(m)
 
-folium.Choropleth(
+choropleth = folium.Choropleth(
     geo_data=regions,
     name="Choropleth",
     data=education_data,
     columns=["Geography", "2017 Male", "2017 Female"],
     key_on="feature.properties.F15Regions",
-    fill_color="YlGn",
+    fill_color="PuBuGn",
     fill_opacity=0.6,
     line_opacity=0.2,
     legend_name="Education among Genders %",
 ).add_to(m)
 
-# folium.LayerControl().add_to(m)
+folium.LayerControl().add_to(m)
+choropleth.geojson.add_child(
+    folium.features.GeoJsonTooltip(["F15Regions"], labels=False)
+)
+
 m.save("./templates/maps/education_map.html")
 
